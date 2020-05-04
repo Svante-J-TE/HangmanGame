cee1293c3ef8edaaae06fa0c3826f43647e8a6f7
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-05-04 14:03:42 +0200

    need to fix so all activities close on quit

--- app/src/main/java/main/game/hangman/MainWinPopUp.java
+++ app/src/main/java/main/game/hangman/MainWinPopUp.java
@@ -2,10 +2,16 @@ package main.game.hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainWinPopUp extends AppCompatActivity {
+    private static TextView displayFinalWord;
+    private MainModel importantData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +25,19 @@ public class MainWinPopUp extends AppCompatActivity {
         int height = dm.heightPixels;
 
         getWindow().setLayout((int)(width*0.8), (int)(height*0.6));
+
+        displayFinalWord = (TextView) findViewById(R.id.finalWordDisplay);
+        importantData = new MainModel();
+
+        displayFinalWord.setText(importantData.get_finalWord());
+
+        final Button buttonReturn = findViewById(R.id.returnButton);
+        buttonReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainWinPopUp.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
