8a813513dea9fd6deb8a99d8897ab7919d1a9cb9
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-05-08 09:14:40 +0200

    javaDoc on all methods in class MainGame

--- app/src/main/java/main/game/hangman/MainGame.java
+++ app/src/main/java/main/game/hangman/MainGame.java
@@ -70,16 +70,25 @@ public class MainGame extends AppCompatActivity {
 
     }
 
+    /**
+     * A method used to randomize a word from a wordList
+     */
     private static void randomizeFinalWord(){
         if (importantData.get_difficulty() == 1){
             Random random = new Random();
             int index = random.nextInt(importantData.get_wordList().size());
             // importantData.set_finalWord(importantData.get_wordList().get(index));
-            importantData.set_finalWord("hej"); //ta bort detta
+            importantData.set_finalWord("hej"); //TODO, ta bort detta
         }
 
     }
 
+    /**
+     * used to put a correct guess in the user display
+     * @param word
+     * @param guess
+     * @param amountOfLetters
+     */
     private static void placeCharInWord(String word, char guess, int amountOfLetters){
         for (int i = 0; i < word.toCharArray().length; i++){
             if(word.toCharArray()[i] == guess){
@@ -94,6 +103,11 @@ public class MainGame extends AppCompatActivity {
         }
     }
 
+    /**
+     * used to find the length of a word
+     * @param word the word you want to find the amount of letters for
+     * @return amountOfLetters , the number of letters is returned as an integer
+     */
     private static int findCharAmountOfWord(String word){
         int amountOfLetters = 0;
         for (int i = 0; i < word.toCharArray().length; i++){
@@ -102,6 +116,12 @@ public class MainGame extends AppCompatActivity {
         return amountOfLetters;
     }
 
+    /**
+     * Used to give the word display the same length as the word
+     * @param amountOfLetters used for the for-loop inside the method
+     * @param displayWordForUser the user display
+     * @param containerForWordDisplay a middleman for the displayWordForUser variable
+     */
     private static void lengthOfWordDisplay(int amountOfLetters, TextView displayWordForUser, ArrayList<String> containerForWordDisplay){
         for (int i = 0; i < amountOfLetters; i++){
             containerForWordDisplay.add(" _ ");
@@ -109,6 +129,15 @@ public class MainGame extends AppCompatActivity {
         }
     }
 
+    /**
+     * This method is used to determine what the users guess means for the game.
+     * @param finalWord the word the user is trying to guess the letters of, used to compare letters between itself and guess
+     * @param amountOfLetters used to store the length of the word
+     * @param amountOfWrongGuesses used to store the number of wrong guesses the user has committed
+     * @param alreadyGuessedCharacters used to keep track of already guessed letters
+     * @param wrongGuesses used to keep track of all wrong letters guessed
+     * @return amountOfWrongGuesses
+     */
     private static int userGuessesCharInWord(String finalWord, int amountOfLetters, int amountOfWrongGuesses, ArrayList<String> alreadyGuessedCharacters, ArrayList<String> wrongGuesses){
         //TODO, change to string
         char guess;
@@ -118,26 +147,26 @@ public class MainGame extends AppCompatActivity {
             guess = Character.toUpperCase(guess);
             finalWord = finalWord.toUpperCase();
 
-            if(!Character.toString(guess).matches("[a-zA-Z]+")){
-                //add message about you have to guess a letter and not numbers and what not
-                return amountOfWrongGuesses;
+            if(!Character.toString(guess).matches("[a-zA-Z]+")){ //checks if the users guess is a not a letter
+                //TODO, add message about you have to guess a letter and not numbers and what not
+                return amountOfWrongGuesses; // if its not a letter the method is ended early since anything but letters are unacceptable
             }
 
-            if(finalWord.indexOf(guess) != -1) {
+            if(finalWord.indexOf(guess) != -1) { // checks if the guess is a part of the word, if .indexOf() returns -1 if the guess is not a part of the word
                 if (alreadyGuessedCharacters.contains(guess)){
-                    //add meseage about about already guessing that char
+                    //TODO, add meseage about about already guessing that char
                 }
                 else{
                     placeCharInWord(finalWord, guess, amountOfLetters);
                 }
             }
             else{
-                if (alreadyGuessedCharacters.contains(Character.toString(guess))){
-                    //add meseage about about already guessing that char
+                if (alreadyGuessedCharacters.contains(Character.toString(guess))){ //checks if character has already been guessed
+                    //TODO, add meseage about about already guessing that char
                     return amountOfWrongGuesses;
                 }
                 else {
-                    //message about wrong char + add to array of wrong chars
+                    //TODO, message about wrong char + add to array of wrong chars
                         placeInWrongDisplay(amountOfWrongGuesses, wrongGuesses, guess);
                         alreadyGuessedCharacters.add(Character.toString(guess));
                         amountOfWrongGuesses = amountOfWrongGuesses + 1;
@@ -147,6 +176,13 @@ public class MainGame extends AppCompatActivity {
         return amountOfWrongGuesses;
     }
 
+    /**
+     * This method is used to put wrong answers in an arrayList and to display them to the user
+     * @param amountOfWrongGuesses when the method is used this integer goes up by one, meaning the user guessed wrong one more time
+     * @param wrongGuesses this arrayList is used to store all the wrong guesses for controll of later guesses
+     * @param guess the guess the user put in is stored here
+     * @return
+     */
     private static int placeInWrongDisplay(int amountOfWrongGuesses, ArrayList<String> wrongGuesses, char guess){
         wrongGuesses.add(Character.toString(guess));
 
@@ -156,6 +192,10 @@ public class MainGame extends AppCompatActivity {
         return amountOfWrongGuesses;
     }
 
+    /**
+     * A method used after every wrong guess from the user. Works as a display of how many wrong guesses the user has commited.
+     * @param amountOfWrongGuesses this integer is used to track which image is supposed to be shown
+     */
     private static void displayHangman(int amountOfWrongGuesses){
         if (amountOfWrongGuesses == 1){
             hangmanDisplay.setImageResource(R.drawable.hangman_image1);
@@ -193,6 +233,10 @@ public class MainGame extends AppCompatActivity {
 
     }
 
+    /**
+     * Method launches different activities depending on the boolean winOrLose
+     * @param winOrLose If the boolean is true that means the player has won the game
+     */
     private void launchPlayerWinOrLoseActivity(boolean winOrLose){
         Intent intent;
         if (winOrLose == true){
